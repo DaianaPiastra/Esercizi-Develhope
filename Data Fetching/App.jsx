46d6a383432ea9e0fe9938d3bdaad8f04fc5f464
Daiana Piastra <160520187+DaianaPiastra@users.noreply.github.com>
2024-05-27 18:23:00 +0200

    Add Data Fetching

---
+++ Data Fetching/App.jsx
@@ -0,0 +1,22 @@
+
+
+import { GithubUser } from "./GithubUser"
+import{Welcome} from "./Welcome"
+
+
+
+
+export function App() {
+  
+    
+    return (
+        <div>
+          <Welcome/>
+          <GithubUser username="your_github_username"/>
+        </div>
+       
+         
+    )
+}
+
+
