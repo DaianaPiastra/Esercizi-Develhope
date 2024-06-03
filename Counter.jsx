bcdc7f33ec86cfc1f70fe4e07d0d9ec60072ddbf
Daiana Piastra <160520187+DaianaPiastra@users.noreply.github.com>
2024-06-03 20:30:35 +0200

    Add Counter of ref exercise

--- Counter.jsx
+++ Counter.jsx
@@ -1,23 +1,36 @@
-import { useState } from "react"
+import { useState, useRef, useEffect } from "react";
 
-export function Counter({initialValue = 0}) {
-    const [counter, setCounter]= useState(initialValue)
-    const handlerIncrement= ()=>{
-        setCounter((c) => c+1)
-    }
-    const handlerDecrement= ()=>{
-        setCounter((c)=> c-1)
-    }
-    const handlerReset= ()=> {
-        setCounter(initialValue)
-    }
+export function Counter({ initialValue = 0 }) {
+    const [counter, setCounter] = useState(initialValue);
+    const directionRef = useRef(null);
+
+    const handlerIncrement = () => {
+        setCounter((c) => c + 1);
+    };
+
+    const handlerDecrement = () => {
+        setCounter((c) => c - 1);
+    };
+
+    const handlerReset = () => {
+        setCounter(initialValue);
+    };
+
+    useEffect(() => {
+        const newDirection = counter > initialValue ? "up" : counter < initialValue ? "down" : "reset"
+
+        if (directionRef.current !== newDirection) {
+            directionRef.current = newDirection;
+            console.log(directionRef.current);
+        }
+    }, [counter, initialValue]);
 
     return (
         <div>
-            <h2>I have counted to {counter}</h2>
-            <button onClick={handlerIncrement}>Increment</button>
-            <button onClick={handlerDecrement}>Decrement</button>
+            <h2>Ho contato fino a {counter}</h2>
+            <button onClick={handlerIncrement}>Incrementa</button>
+            <button onClick={handlerDecrement}>Decrementa</button>
             <button onClick={handlerReset}>Reset</button>
         </div>
-    )
+    );
 }
